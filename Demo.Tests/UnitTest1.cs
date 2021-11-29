4ed9e2a347b5872c6666325d86406dfa84239a04
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2021-11-29 17:58:50 +0300

    Add project files.

---
+++ Demo.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Demo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var olcumler = new List<Olcum>()
+            {
+                new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                }
+            };
+            var gruplayici = new Gruplayici(1);
+            var gruplar = gruplayici.Grupla(olcumler);
+
+            Assert.AreEqual(1, gruplar.Count);
+        }
+    }
+}
