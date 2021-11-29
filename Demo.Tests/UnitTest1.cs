a03e9e2dee1a1601532f40cff5f81d5bafff0be3
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2021-11-29 18:23:28 +0300

    Green & Refactor

--- Demo.Tests/UnitTest1.cs
+++ Demo.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Demo.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void bir_elemanli_liste_birerli_gruplanmak_istediğinde_grup_sayisi_bir_olmalidir()
         {
             var olcumler = new List<Olcum>()
             {
@@ -22,6 +22,48 @@ namespace Demo.Tests
             var gruplar = gruplayici.Grupla(olcumler);
 
             Assert.AreEqual(1, gruplar.Count);
+        }
+
+        [TestMethod]
+        public void alti_elemanli_liste_ikiserli_gruplanmak_istediğinde_grup_sayisi_uc_olmalidir()
+        {
+            var olcumler = new List<Olcum>()
+            {
+                new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                },
+                new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                },
+                new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                },
+                new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                },
+                new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                },
+                new Olcum
+                {
+                    EnYuksek = 10,
+                    EnDusuk = 1
+                }
+            };
+            var gruplayici = new Gruplayici(2);
+            var gruplar = gruplayici.Grupla(olcumler);
+
+            Assert.AreEqual(3, gruplar.Count);
         }
     }
 }
